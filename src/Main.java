f6b072d9a06e12a99edd46686d2eebced71b5fa7
BerGer23 <berendkrebs@gmail.com>
2013-11-15 11:42:06 +0100

    16

--- src/Main.java
+++ src/Main.java
@@ -415,6 +415,15 @@ public class Main {
 	 */
 
 	private static void problem15() {
+		System.out.println("lattice ways");
+
+		boolean field[][] = new boolean[20][20];
+
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
+				field[i][j] = false;
+			}
+		}
 
 	}
 
